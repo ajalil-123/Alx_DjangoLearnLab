1950bc0f0681dbeba1179b5af4f8ecb6142d4ee8
Jalil <abduljalilzakaria1@gmail.com>
2025-03-02 00:26:17 +0200

    applied advanced relationship models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -11,8 +11,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return author.books.all()  # Returns all books by the author
+    author = Author.objects.get(name=author_name)  # Get the author instance
+    return Book.objects.filter(author=author)  # Use filter() to get all books by this author
 
 # List all books in a library
 def get_books_in_library(library_name):
@@ -21,7 +21,8 @@ def get_books_in_library(library_name):
 
 # Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian  
+    library = Library.objects.get(name=library_name)  # Get the library instance
+    return library.librarian  # Fetch the related librarian
+
 
 
