9b858ee06a80f03086145413f98539cd4c4cba4b
Jalil <abduljalilzakaria1@gmail.com>
2025-03-03 17:24:32 +0200

    added authentication features to rel_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from .views import list_books,LibraryDetailView
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import SignUpView
+from .views import register
 
 
 urlpatterns = [
     path('', view=list_books, name='books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path("register/", SignUpView.as_view(), name="register"), 
+    path("register/", register, name="register"), 
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -56,7 +56,15 @@ class CustomLoginView(LoginView):
 class CustomLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "relationship_app/registration.html"
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user automatically after registration
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    
+    return render(request, "relationship_app/registration.html", {"form": form})
