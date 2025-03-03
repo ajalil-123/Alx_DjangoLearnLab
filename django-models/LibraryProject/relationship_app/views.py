aa1cda57914507bf6d3499bd69f66839654565d5
Jalil <abduljalilzakaria1@gmail.com>
2025-03-03 17:03:53 +0200

    added authentication features to rel_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -47,6 +47,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
 
 
 class CustomLoginView(LoginView):
