c025c345c9201237c6181b9b87fd7944d408549b
Jalil <abduljalilzakaria1@gmail.com>
2025-03-05 15:04:46 +0200

    Api development

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    tittle = models.CharField(max_length=225)
+    title = models.CharField(max_length=225)
     author = models.CharField(max_length=225)
