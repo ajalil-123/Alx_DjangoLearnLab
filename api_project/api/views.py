f461d63889ee3c98800c172d13a7a6d9a6352889
Jalil <abduljalilzakaria1@gmail.com>
2025-03-05 14:57:09 +0200

    Api development

---
+++ api_project/api/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+
